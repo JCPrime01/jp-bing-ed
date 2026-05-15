d68a5ef6227ef18ebcac216f33f291b801d861c5
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-15 11:49:14 -0300

    chore: update shareCode to 3NAS17RJ28O

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/hncv0ue6?shareCode=W2OIEFMSGW7&afp=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/hncv0ue6?shareCode=3NAS17RJ28O&afp=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
