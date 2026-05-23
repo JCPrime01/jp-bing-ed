b58b309de207ad95cc847bd2269451cc730b134c
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-23 09:46:33 -0300

    chore: update shareCode to XJIRW0IDDUY

--- api/index.js
+++ api/index.js
@@ -26,6 +26,6 @@ export default async function handler(req, res) {
 
   const sub1 = req.query.sub1 || req.query.fbclid || "";
   const sub1Param = sub1 ? "&sub1=" + sub1 : "";
-  res.writeHead(302, { Location: "https://go.aff.esportiva.bet/hncv0ue6?shareCode=QGD2Z5981GU&afp=bingo" + sub1Param });
+  res.writeHead(302, { Location: "https://go.aff.esportiva.bet/hncv0ue6?shareCode=XJIRW0IDDUY&afp=bingo" + sub1Param });
   res.end();
 }
