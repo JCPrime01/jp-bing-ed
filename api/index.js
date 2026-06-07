eae08167a6d5992a07f13ad28f6f8a16d018d688
JCPrime01 <vanzokiller@gmail.com>
2026-06-07 09:45:20 -0300

    chore: shareCode BQHOQHGIHI9 -> KACFW31AUJK (via painel)

--- api/index.js
+++ api/index.js
@@ -26,6 +26,6 @@ export default async function handler(req, res) {
 
   const sub1 = req.query.sub1 || req.query.fbclid || "";
   const sub1Param = sub1 ? "&sub1=" + sub1 : "";
-  res.writeHead(302, { Location: "https://go.aff.esportiva.bet/hncv0ue6?shareCode=BQHOQHGIHI9&afp=bingo" + sub1Param });
+  res.writeHead(302, { Location: "https://go.aff.esportiva.bet/hncv0ue6?shareCode=KACFW31AUJK&afp=bingo" + sub1Param });
   res.end();
 }
