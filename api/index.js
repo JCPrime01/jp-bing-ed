42fa05101bec95dc5cadbc76fd5c7feda8ec3f45
JCPrime01 <vanzokiller@gmail.com>
2026-06-15 18:01:37 -0300

    chore: shareCode BM2QP85LISS -> G8U9KQ1QH60 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@ export default async function handler(req, res) {
     return;
   }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7181&shareCode=BM2QP85LISS&afp1=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7181&shareCode=G8U9KQ1QH60&afp1=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
