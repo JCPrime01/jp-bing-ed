866e19d47ca92ab3150db07389c7d82930f3c748
JCPrime01 <vanzokiller@gmail.com>
2026-06-25 10:20:13 -0300

    chore: shareCode 5TW9KRSCZZO -> EVW4RJIXZFJ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@ export default async function handler(req, res) {
     return;
   }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7181&shareCode=5TW9KRSCZZO&afp1=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7181&shareCode=EVW4RJIXZFJ&afp1=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
