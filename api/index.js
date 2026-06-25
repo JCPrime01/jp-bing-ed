c7835fbb4096640a4a36dbd9374648d4429383ba
JCPrime01 <vanzokiller@gmail.com>
2026-06-25 09:47:39 -0300

    chore: shareCode VVAYGCEDT0P -> EVW4RJIXZFJ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@ export default async function handler(req, res) {
     return;
   }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7181&shareCode=VVAYGCEDT0P&afp1=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7181&shareCode=EVW4RJIXZFJ&afp1=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
