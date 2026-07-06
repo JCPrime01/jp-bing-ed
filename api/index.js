5f56013ae475ca3093801d37d3bf240051b4cdea
JCPrime01 <vanzokiller@gmail.com>
2026-07-06 10:06:59 -0300

    chore: shareCode XW6PNFUAM5N -> G5M6JWQG41K (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@ export default async function handler(req, res) {
     return;
   }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7181&shareCode=XW6PNFUAM5N&afp1=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7181&shareCode=G5M6JWQG41K&afp1=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
