228e870cddc924809bff4830bd971f2a2bc8769d
JCPrime01 <vanzokiller@gmail.com>
2026-07-11 09:20:16 -0300

    chore: shareCode F22U59TO5ZD -> QHCW6VJQNLR (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@ export default async function handler(req, res) {
     return;
   }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7181&shareCode=F22U59TO5ZD&afp1=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7181&shareCode=QHCW6VJQNLR&afp1=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
