2fd5a791016ae69d5a4d23fece8523fc9a336256
JCPrime01 <vanzokiller@gmail.com>
2026-07-16 14:37:35 -0300

    chore: shareCode FQ4WOEYM116 -> NKKRLOSDSBP (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@ export default async function handler(req, res) {
     return;
   }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7181&shareCode=FQ4WOEYM116&afp1=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7181&shareCode=NKKRLOSDSBP&afp1=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
