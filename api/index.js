31d8b004f25deeef3da155336dc7df0063c6e59f
JCPrime01 <vanzokiller@gmail.com>
2026-07-17 14:33:10 -0300

    chore: shareCode ALI0VV4G5VR -> HOAWFX2Z3RR (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@ export default async function handler(req, res) {
     return;
   }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7181&shareCode=ALI0VV4G5VR&afp1=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7181&shareCode=HOAWFX2Z3RR&afp1=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
