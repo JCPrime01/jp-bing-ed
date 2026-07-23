cf53217d43f242015dd1cf4254c2c9f461a501ea
JCPrime01 <vanzokiller@gmail.com>
2026-07-23 12:07:00 -0300

    chore: shareCode 9SZAGC9EHDS -> NA60UDLMFK6 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@ export default async function handler(req, res) {
     return;
   }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7181&shareCode=9SZAGC9EHDS&afp1=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7181&shareCode=NA60UDLMFK6&afp1=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
