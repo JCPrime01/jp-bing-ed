0fcab2b19fd7b009cfa2c4e2373dae56def7ceb4
JCPrime01 <vanzokiller@gmail.com>
2026-08-12 12:02:35 -0300

    chore: shareCode BSE0D76N6OK -> IOW718QF83V (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@ export default async function handler(req, res) {
     return;
   }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7181&shareCode=BSE0D76N6OK&afp1=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7181&shareCode=IOW718QF83V&afp1=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
